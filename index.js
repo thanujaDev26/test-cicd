6c1eb4050e5ca38e9ea9ade969d5d2ca031f6172
Thanuja Priyadarshane <thanujapriyadarshane@Thanujas-MacBook-Air.local>
2024-12-22 11:29:47 +0530

    Demo GET request is added

--- index.js
+++ index.js
@@ -7,6 +7,13 @@ const port = process.env.PORT
 
 console.log(port)
 
+app.get('/api/message', (req, res) =>{
+    res.status(200).json({
+        statusbar: 'success',
+        message: 'Welcome to the API!'
+    })
+})
+
 app.listen(port,'0.0.0.0',()=>{
     console.log(`Server is running on port ${port}`)
 })
